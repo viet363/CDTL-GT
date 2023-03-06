2e73976235b152eaffb31eb9b888c27bd2be9684
viet363 <110683284+viet363@users.noreply.github.com>
2023-03-06 22:21:44 +0700

    Add files via upload

---
+++ tongchan.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+#define  Max100
+void NhapMang(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    { 	cout<<"phan tu thu  "<<i<<"=";
+        cin>>a[i];
+    }
+}
+
+void XuatMang(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout<< a[i];
+    }
+}
+int tongchan(int a[], int n, int i=0,int tong =0)
+{ 	
+	if(i>=n){
+		return tong;
+		}
+	if(a[i] % 2==0){
+		tong +=a[i];
+		}
+	return tongchan(a,n,++i,tong) ;
+}
+int main()
+{
+    int n; 
+    do
+    {
+        cout<<"Nhap so phan tu mang:";
+        cin>>n;
+    } while (n <= 0  );
+	    int a[n];
+	    NhapMang(a,n);
+    cout<<"Tong so chan ="<<tongchan(a, n);
+}
